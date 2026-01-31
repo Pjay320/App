7febc05c2336d828dc51fa785fca1063a912e528
pjay320 <pjaypl11@gmail.com>
2026-01-31 14:19:07 +0100

    Implementacja API, UI i SignalR

---
+++ Server/Controllers/ResourceController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Shared;
+
+namespace Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ResourcesController : ControllerBase
+{
+    // Udawana baza danych (Singleton)
+    private static List<Resource> _db = new List<Resource>
+    {
+        new Resource { Id = 1, Name = "Laptop Dell", Status = "Dostępny" },
+        new Resource { Id = 2, Name = "Projektor", Status = "W naprawie" }
+    };
+
+    private readonly IHubContext<ResourceHub> _hub;
+
+    public ResourcesController(IHubContext<ResourceHub> hub)
+    {
+        _hub = hub;
+    }
+
+    [HttpGet]
+    public IEnumerable<Resource> Get() => _db;
+
+    [HttpPost]
+    public async Task Post(Resource resource)
+    {
+        resource.Id = _db.Any() ? _db.Max(x => x.Id) + 1 : 1;
+        _db.Add(resource);
+        // Powiadom wszystkich klientów przez SignalR (Wymóg 5)
+        await _hub.Clients.All.SendAsync("ReceiveUpdate");
+    }
+}
---
+++ Shared/Resource.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared;
+
+public class Resource
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Status { get; set; } = "Dostępny";
+}
